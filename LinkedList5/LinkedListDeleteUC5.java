871447825edcee2a813781fab7402ebf875c36c3
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-27 19:32:02 +0530

    Added: LinkedList problem 5

---
+++ LinkedList5/LinkedListDeleteUC5.java
@@ -0,0 +1,15 @@
+package com.java;
+import java.util.*;
+public class LinkedListDeleteUC5 {
+
+	public static void main(String[] args) {
+		LinkedList5 list = new LinkedList5();
+        list.insert(56, 0);
+        list.insert(30, 1);
+        list.insert(70, 2);
+        list.deleteFirst();
+        list.printList(); 
+
+	}
+
+}
