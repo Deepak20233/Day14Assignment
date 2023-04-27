6c6b2a21e1734339328056cec2bb42934c345bad
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-27 19:24:57 +0530

    Added: LinkedList problem 3

---
+++ LinkedList3/LinkedListUC3.java
@@ -0,0 +1,12 @@
+package com.java;
+import java.util.*;
+public class LinkedListUC3 extends LinkedList2{
+
+	public static void main(String[] args) {
+		LinkedList2 list = new LinkedList2();
+        list.append(56);
+        list.append(30);
+        list.append(70);
+        list.printList();
+	}
+}
